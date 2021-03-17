e5f985cbc9e892961b8f748322bf8a203dd4ca0c
builderai <80264376+builderai@users.noreply.github.com>
2021-03-17 13:00:51 +0530

    Update croppie.js
    Loader Changed

--- croppie.js
+++ croppie.js
@@ -368,7 +368,7 @@
         boundary = self.elements.boundary = document.createElement('div');
         viewport = self.elements.viewport = document.createElement('div');
         img = self.elements.img = document.createElement('img');
-        img.src = "../../../assets/images/loader_logo.gif";
+        img.src = "../../../assets/images/spinner.gif";
         overlay = self.elements.overlay = document.createElement('div');
 
         if (self.options.useCanvas) {
