b9749b1d66f022da4334ae9e5171f3321f46e2e3
builderai <80264376+builderai@users.noreply.github.com>
2021-03-17 12:06:29 +0530

    Update croppie.js

--- croppie.js
+++ croppie.js
@@ -368,7 +368,7 @@
         boundary = self.elements.boundary = document.createElement('div');
         viewport = self.elements.viewport = document.createElement('div');
         img = self.elements.img = document.createElement('img');
-        img.src = "../../../assets/images/buildernow-logo.svg";
+        img.src = "../../../assets/images/loader_logo.gif";
         overlay = self.elements.overlay = document.createElement('div');
 
         if (self.options.useCanvas) {
